bff805205ac339d1b704ff4c64a16a66151a62ca
alanin006-sketch <alanin006@gmail.com>
2025-09-19 10:59:15 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 // app.js
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const http = require('https');
+const hostname = 'test-vzwv.onrender.com';
+const port = 80;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
